94963533b4951eb51bc344a758eecfae673ffb37
Maksym Sukhovii <43554658+smaxxxs@users.noreply.github.com>
2019-12-04 11:04:37 +0200

    Update Application.java
    extra comments are deleted

--- src/main/java/admission/Application.java
+++ src/main/java/admission/Application.java
@@ -3,7 +3,6 @@ package admission;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
 public class Application {
 	
